baeed25a2a4c27599a0981d595ce3aab5ae5e250
Kuromorimine <bestpbrock@gmail.com>
2024-11-03 08:52:55 +0700

    1

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,32 +2,32 @@ import React from 'react'
 import NumericalNavbar from './Navbar';;
 import "../styles/home.css"
 
-// const menu = [
-//     { label: "Graphical", link: "/Graphical" },
-//     { label: "Bisection", link: "/Bisection" },
-//     { label: "Falseposition", link: "/Falseposition" },
-//     { label: "Onepoint", link: "/Onepoint" },
-//     { label: "NewtonRaphson", link: "/NewtonRaphson" },
-//     { label: "Secant", link: "/Secant" },
-//     { label: "CramerRule", link: "/Cramer" },
-//     { label: "Gaussian",link:"/Gauss"},
-//     { label: "GaussJordan", link:"GaussJordan"},
-//     { label: "MatrixInversion",link:"MatrixInversion"},
-//     { label: "LU",link:"LU"},
-//     { label: "Jacobi",link:"Jacobi"},
-//     { label: "GaussSeidei",link:"GaussSeidel"},
-//     { label: "ConjugateGradient",link:"ConjugateGradient"},
-//     { label: "NewtonDivided", link: "/NewtonDivided"},
-//     { label: "Langange", link:"/Langange"},
-//     { label: "Spline", link:"/Spline"},
-//     { label: "Linear-least-Square-Regression", link:"/Linear"},
-//     { label: "Polynomial-least-Square-Regression", link:"/Polynomial"},
-//     { label: "TrapezoidalRule",link:"/Trapezoidal"},
-//     { label: "CompositeTrapezoidal",link:"/CompositeTrapezoidal"},
-//     { label: "SimpsonRule", link:"/Simpson"},
-//     { label: "CompositeSimpsonRule",link:"/CompositeSimpson"}
+const menu = [
+    { label: "Graphical", link: "/Graphical" },
+    { label: "Bisection", link: "/Bisection" },
+    { label: "Falseposition", link: "/Falseposition" },
+    { label: "Onepoint", link: "/Onepoint" },
+    { label: "NewtonRaphson", link: "/NewtonRaphson" },
+    { label: "Secant", link: "/Secant" },
+    { label: "CramerRule", link: "/Cramer" },
+    { label: "Gaussian",link:"/Gauss"},
+    { label: "GaussJordan", link:"GaussJordan"},
+    { label: "MatrixInversion",link:"MatrixInversion"},
+    { label: "LU",link:"LU"},
+    { label: "Jacobi",link:"Jacobi"},
+    { label: "GaussSeidei",link:"GaussSeidel"},
+    { label: "ConjugateGradient",link:"ConjugateGradient"},
+    { label: "NewtonDivided", link: "/NewtonDivided"},
+    { label: "Langange", link:"/Langange"},
+    { label: "Spline", link:"/Spline"},
+    { label: "Linear-least-Square-Regression", link:"/Linear"},
+    { label: "Polynomial-least-Square-Regression", link:"/Polynomial"},
+    { label: "TrapezoidalRule",link:"/Trapezoidal"},
+    { label: "CompositeTrapezoidal",link:"/CompositeTrapezoidal"},
+    { label: "SimpsonRule", link:"/Simpson"},
+    { label: "CompositeSimpsonRule",link:"/CompositeSimpson"}
 
-// ];
+];
 
 export default function Home() {
     
@@ -35,9 +35,9 @@ export default function Home() {
         
             <div className="main">
                 <NumericalNavbar/>
-                {/* {menu.map((menu, index) => (
+                {menu.map((menu, index) => (
                 <Link to={menu.link}>{menu.label}</Link>
-            ))} */}
+            ))}
             </div>
         
         
